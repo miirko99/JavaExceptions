2f8ae25d31b455dd2ff7ea054440104f1f3ab002
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-09-06 11:43:41 +0200

    Preuredjen main.java tako da se exception prosledjuje kroz pozive funkcija

--- Main.java
+++ Main.java
@@ -1,4 +1,17 @@
 public class Main {
+    public static void fillHeights(double[] heights) throws HeightTooBigException, HeightLessThanZeroException {
+        heights[0]=3456;
+        heights[1]=-3;
+        heights[2]=23;
+        heights[3]=345;//Index out of bounds unchecked
+        checkHeights(heights);//checked HeightTooBigException, HeightLessThanZeroException
+    }
+    public static void checkHeights(double[] heights) throws HeightTooBigException, HeightLessThanZeroException {
+        validateHeight(heights[3]);//Index out of bounds unchecked
+        validateHeight(heights[0]);//checked HeightTooBigException
+        validateHeight(heights[1]);//checked HeightLessThanZeroException
+        validateHeight(heights[2]);//checked but not throw any
+    }
     public static void validateHeight(double Height) throws HeightLessThanZeroException, HeightTooBigException {
         if(Height<=0){
             throw new HeightLessThanZeroException();
@@ -6,21 +19,12 @@ public class Main {
         if(Height>300){
             throw new HeightTooBigException();
         }
-
     }
     public static void main(String[] args){
-        System.out.println("Hello world!");
         double[] heights=new double[3];
-        heights[0]=3456;
-        heights[1]=-3;
-        heights[2]=23;
-        heights[3]=345;
+        //heights[3]=345;//Index out of bounds unchecked
         try {
-            validateHeight(heights[0]);
-            validateHeight(heights[1]);
-            validateHeight(heights[2]);
-            heights[3]=345;//unchecked
-
+            fillHeights(heights);
         } catch (HeightLessThanZeroException | HeightTooBigException | ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
