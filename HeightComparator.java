9bfc809abe2734381adbd6532b1b7cbb1736de59
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-09-07 22:39:06 +0200

    kreirani testovi

--- HeightComparator.java
+++ HeightComparator.java
@@ -17,6 +17,4 @@ public class HeightComparator {
         this.valid.validateHeight(b);
         return a<b;
     }
-
-
 }
--- Main.java
+++ Main.java
@@ -1,14 +1,34 @@
 public class Main {
 
     public static void main(String[] args){
-        double[] heights=new double[3];
+        double[] heights=new double[]{180,185,192};
+        //heights[3]=216;
 
 
+        HeightComparator HC1=new HeightComparator(100,200);
         HeightComparator HC=new HeightComparator();
         try {
-            HC.isGrather(1,2);
+            heights[3]=216;
+            HC.isGrather(1,623);
+            HC.isGrather(-1,2);
+            HC.isGrather(0,2);
+            HC.isGrather(0,2);
+            HC.isLess(1,623);
+            HC.isLess(-1,2);
+            HC.isLess(0,2);
+            HC.isLess(-12341,2);
+            System.out.println(HC.isLess(1,2));
+            HC1.isGrather(101,623);
+            HC1.isGrather(50,150);
+            HC1.isGrather(100,199);
+            HC1.isGrather(101,102);
+            HC1.isLess(101,623);
+            HC1.isLess(50,150);
+            HC1.isLess(-1,160);
+            HC1.isLess(-12341,243);
         } catch (HeightLessThanZeroException | HeightTooBigException | ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
+        //HC.isGrather(2,3);checked
     }
 }
