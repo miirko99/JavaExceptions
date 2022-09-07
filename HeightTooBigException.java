bb6a4b3240ec2191588daed5103c45a249024eaa
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-09-07 22:12:28 +0200

    dodat zarez(nebitno)

--- HeightTooBigException.java
+++ HeightTooBigException.java
@@ -2,7 +2,7 @@ public class HeightTooBigException extends Exception {
     public HeightTooBigException(){
         super("Prevelika visina");    }
     HeightTooBigException(double H){
-        super("prevelika visina vas unos: "+H);
+        super("prevelika visina, vas unos: "+H);
     }
     public HeightTooBigException(String msg){
         super("custom"+msg);
