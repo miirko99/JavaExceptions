8a5621e53e93da1a85bc41d7b07848a19978fef9
Mirko Petrovic <mirkic.kg99@gmail.com>
2022-09-08 11:04:31 +0200

    dodati testovi za checkHeights

--- HeightValidatorTest.java
+++ HeightValidatorTest.java
@@ -31,6 +31,7 @@ class HeightValidatorTest {
     }
     @Test
     void GratherThanHighSouldThrowHTBE(){
+        assertThrows(HeightTooBigException.class,()->{HV.validateHeight(215.00000001);});
         assertThrows(HeightTooBigException.class,()->{HV.validateHeight(216);});
         assertThrows(HeightTooBigException.class,()->{HV.validateHeight(2000);});
         LOGGER.log(Level.INFO, "HeightTooBig working correct");
@@ -41,5 +42,24 @@ class HeightValidatorTest {
         assertTrue(HV.validateHeight(178));
         assertTrue(HV.validateHeight(215));
         LOGGER.log(Level.INFO, "No exception working correct");
+    }
+    @Test
+    void checkHeightsTooBig(){
+        assertThrows(HeightTooBigException.class,()->{HV.checkHeights(new double[]{123,432,150});});
+        assertThrows(HeightTooBigException.class,()->{HV.checkHeights(new double[]{215.0001,99,150});});
+        assertThrows(HeightTooBigException.class,()->{HV.checkHeights(new double[]{215,150,250});});
+        LOGGER.log(Level.INFO, "CheckHeights TooBig exception working correct");
+    }
+    @Test
+    void checkHeightsTooLow(){
+        assertThrows(HeightLessThanLowException.class,()->{HV.checkHeights(new double[]{99.99,432,150});});
+        assertThrows(HeightLessThanLowException.class,()->{HV.checkHeights(new double[]{182,150,45});});
+        assertThrows(HeightLessThanLowException.class,()->{HV.checkHeights(new double[]{150,75,250});});
+        LOGGER.log(Level.INFO, "CheckHeights TooLow exception working correct");
+    }
+    @Test
+    void checkHeightsTrue() throws HeightTooBigException, HeightLessThanLowException {
+        assertTrue(HV.checkHeights(new double[]{100,150,215}));
+        LOGGER.log(Level.INFO, "CheckHeights no exception working correct");
     }
 }
